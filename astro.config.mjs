6d1f3fc7357b922bfdc48f6b7683b752dd6be6fd
0xh3x0x <kamleshkathiriya001@gmail.com>
2026-06-17 20:04:46 +0530

    bug fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,8 +26,8 @@ import rehypePixelated from './src/plugins/rehype-pixelated' /* Custom plugin to
 
 // https://astro.build/config
 export default defineConfig({
-  //site: siteConfig.site,
-  site: 'https://0xh3x.github.io',
+  site: siteConfig.site,
+  //site: 'https://0xh3x.github.io',
   base: '/0xh3x.github.io',
   trailingSlash: siteConfig.trailingSlashes ? 'always' : 'never',
   prefetch: true,
